e47aa18982cc12d6b817a81e2211db1874cd3326
wenbinJ <157052371+Wenbin-Jiang-Section-E@users.noreply.github.com>
2024-09-12 19:41:44 -0400

    fs and path

---
+++ test.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const path = require("path");
+
+// const mypath = path.join(__dirname, "pages", "home.html");
+// console.log(mypath);
+
+//read file
+// const file = path.join(__dirname, "example.txt");
+// fs.readFile(file, "utf-8", (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
+// });
+
+//read directory
+fs.readdir(path.join(__dirname, "pages"), "utf-8", (err, data) => {
+  if (err) throw err;
+  console.log("file array", data);
+
+  data.forEach((item) => {
+    console.log("file names", item);
+    fs.readFile(path.join(__dirname, "pages", item), "utf-8", (err, data) => {
+      if (err) throw err;
+      console.log(`file content ${data}`);
+    });
+  });
+});
